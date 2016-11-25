b8530ffe6dc203bbb230153d7e15bfb959012c70
Adam Renberg <tgwizard@gmail.com>
2016-11-25 23:04:46 +0200

    Simple game join/leave and player update events

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var express = require('express');
+var _ = require('lodash');
 
 var app = express();
 var server = http.createServer(app);
@@ -11,4 +12,69 @@ var port = process.env.PORT || 5000;
 server.listen(port, function(err) {
   if (err) return console.log(err.stack);
   console.log('listening on port', port);
-})
+});
+
+
+games = {};
+
+
+function getOrCreateGame(gameId) {
+  if (!games[gameId]) {
+    games[gameId] = {
+      'id': gameId,
+      'players': [],
+      'missiles': []
+    }
+  }
+  return games[gameId];
+}
+
+function createPlayer(username, socket) {
+  return {
+    username: username,
+    socket: socket,
+    calibrations: {},
+  };
+}
+
+function addPlayer(game, player) {
+  game.players.push(player);
+  publishPlayersUpdated(game);
+}
+
+function removePlayer(game, username) {
+  // TODO...
+  console.log('removing player', username, 'from game', game.id)
+  game.players = _.reject(game.players, function(p) {
+    return p.username == username;
+  })
+  publishPlayersUpdated(game);
+}
+
+function publishPlayersUpdated(game) {
+  var playerData = _.map(game.players, function(p) { return {username: p.username} });
+  console.log('player data', playerData)
+  game.players.forEach(function(p) {
+    p.socket.emit('players updated', {players: playerData});
+  });
+}
+
+io.on('connection', function(socket) {
+  console.log('user connected');
+
+  var username, game;
+
+  socket.on('join game', function(data) {
+    console.log('join game', data);
+    username = data.username;
+
+    game = getOrCreateGame(data.gameId);
+    addPlayer(game, createPlayer(username, socket));
+  });
+
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+    if (!game) return;
+    removePlayer(game, username)
+  });
+});
--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,24 @@
 (function() {
   document.getElementById('hello').innerText = 'Hello world';
+
+  var gameIdInput = $("#game-id");
+  var usernameInput = $("#username");
+  $("#start-game").on("submit", function(e) {
+    e.preventDefault();
+
+    var gameId = gameIdInput.val()
+    var username = usernameInput.val();
+    if (!gameId || !username) {
+      alert('BOO');
+      return
+    }
+
+    socket.emit('join game', {'gameId': gameId, 'username': username})
+  })
+
+  var socket = io();
+
+  socket.on('players updated', function(data) {
+    console.log('players updated', data);
+  });
 })();
