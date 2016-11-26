776b694219c6707e14b655ffa8d366e17905f9eb
par.dahlman <par.dahlman@fellowdeveloper.se>
2016-11-26 13:40:41 +0100

    Romve outcommented code

--- static/js/app.js
+++ static/js/app.js
@@ -1,28 +1,3 @@
-
-// $("#start-game").on('click', function(e) {
-//   socket.emit('start game')
-// })
-//
-// $("#set-calibration").on('click', function(e) {
-//   socket.emit('set calibration', orientation);
-// });
-//
-// $("#fire-missile").on('click', function(e) {
-//   socket.emit('fire missile', {direction: orientation.z});
-// });
-
-// socket.on('players updated', function(data) {
-//   console.log('players updated', data);
-// });
-//
-// socket.on('start calibration', function(data) {
-//   console.log('start calibration', data);
-// });
-//
-// socket.on('game started', function(data) {
-//   console.log('game started', data);
-// });
-
 class JoinGameForm extends React.Component {
 
   constructor(props) {
